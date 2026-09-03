f658bd1c7b6689f9ece4dcd4a56e329bfbefea39
hyika <d3armyfriends@icloud.com>
2026-09-03 14:23:12 +0900

    main에 있는 gateway 관련 코드를 전부 'gateway.rs' 파일로 분리하였습니다.

--- src/gateway.rs
+++ src/gateway.rs
@@ -1,58 +1,214 @@
 use futures_util::{SinkExt, StreamExt, stream::{SplitSink, SplitStream}};
-use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::WebSocketStream;
-use tokio_tungstenite::MaybeTlsStream;
-use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, WebSocketStream, tungstenite::protocol::Message};
+use tokio::{sync::Mutex, net::TcpStream};
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
+use std::{f32::consts::E, sync::Arc};
 
+
+#[derive(Serialize_repr, Debug)]
+#[repr(u8)]
+pub enum OutboundOpcode {
+    Heartbeat = 1,
+    Identify = 2,
+    PresenceUpdate = 3,
+    VoiceStateUpdate = 4,
+    Resume = 6,
+    RequestGuildMembers = 8,
+    RequestSoundboardSounds = 31,
+}
+
+// user -> discord
+#[derive(Debug, Serialize)]
+pub struct OutboundPayload<T> {
+    op: OutboundOpcode, 
+    d: T,
+}
+
+impl<T> OutboundPayload<T> {
+    pub fn to_message(&self) -> Result<Message, Box<dyn std::error::Error + Send + Sync>>
+    where
+        T: Serialize,
+    {
+        let json = serde_json::to_string(self)?;
+        Ok(Message::Text(json.into()))
+    }
+}
+
+#[derive(Deserialize_repr, Debug)]
+#[repr(u8)]
+pub enum InboundOpcode {
+    Dispatch = 0,
+    Reconnect = 7,
+    InvalidSession = 9,
+    Hello = 10,
+    HeartbeatAck = 11,
+}
+
+// discord -> user
+#[derive(Debug, Deserialize)]
+pub struct InboundPayload {
+    pub op: InboundOpcode, 
+    pub d: serde_json::Value,
+    pub s: Option<u64>, 
+    pub t: Option<String>, 
+}
+
+// OPCOCDE 2 :: Identify
+#[derive(Debug, Serialize)]
+pub struct IdentifyData {
+    token: String,
+    intents: u64,
+    properties: IdentifyProperties,
+}
+
+#[derive(Debug, Serialize)]
+pub struct IdentifyProperties{
+    os: String,
+    browser: String,
+    device: String,
+}
 
 pub struct Gateway {
     token: String, 
     url: String, 
-    session_id: Option<String>, 
-    sequence_num: Option<u64>, 
+    session_id: Arc<Mutex<Option<String>>>, 
+    sequence_num: Arc<Mutex<Option<u64>>>, 
 }
 
 impl Gateway {
-    fn new(token: String) -> Gateway {
-        Gateway { 
+    pub fn new(token: String) -> Gateway {
+        Gateway {
             token, 
             url: "wss://gateway.discord.gg/?v=10&encoding=json".to_string(), 
-            session_id: None, 
-            sequence_num: None, 
+            session_id: Arc::new(Mutex::new(None)),
+            sequence_num: Arc::new(Mutex::new(None)),
         }
     }
 
     pub async fn connect(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let (ws_stream, _) = tokio_tungstenite::connect_async(&self.url).await?;
+        let token = self.token.clone();
+
+        let (ws_stream, _) = connect_async(&self.url).await?;
+
         let (mut write, mut read) = ws_stream.split();
+        
+        let (tx, mut rx) = tokio::sync::mpsc::channel::<OutboundOpcode>(32);
 
-        // Send Identify payload
-        let identify_payload = create_identify(&self.token);
-        send_payload(&mut write, &identify_payload).await?;
+        let write_seq_num = Arc::clone(&self.sequence_num);
+        let read_seq_num = Arc::clone(&self.sequence_num);
 
-        // Handle incoming messages
-        while let Some(message) = read.next().await {
-            match message? {
-                Message::Text(text) => {
-                    let inbound: InboundPayload = serde_json::from_str(&text)?;
-                    println!("Received: {:?}", inbound);
-                }
-                Message::Close(_) => {
-                    println!("Connection closed");
-                    break;
-                }
-                _ => {}
+        let sending_task = tokio::spawn(async move {
+            while let Some(op) = rx.recv().await {
+                let message = match op {
+                    OutboundOpcode::Identify => OutboundPayload {
+                            op: OutboundOpcode::Identify,
+                            d: IdentifyData {
+                                token: token.clone(),
+                                intents: 513, // Example intents
+                                properties: IdentifyProperties {
+                                    os: "linux".to_string(),
+                                    browser: "are_you_titti".to_string(),
+                                    device: "are_you_titti".to_string(),
+                                },
+                            },
+                        }.to_message(),
+                    OutboundOpcode::Heartbeat => OutboundPayload {
+                            op: OutboundOpcode::Heartbeat,
+                            d: write_seq_num.lock().await.clone(),
+                        }.to_message(), 
+                    _ => continue, 
+                    // OutboundOpcode::PresenceUpdate => {}
+                    // OutboundOpcode::VoiceStateUpdate => {}
+                    // OutboundOpcode::Resume => {}
+                    // OutboundOpcode::RequestGuildMembers => {}
+                    // OutboundOpcode::RequestSoundboardSounds => {}
+                };
+                println!("Sending message: {:?}", message);
+                write.send(message?).await?;
             }
-        }
 
+            Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+        }); 
+
+        let listening_task = tokio::spawn(async move {
+            while let Some(message) = read.next().await {
+                match message {
+                    Ok(Message::Text(message)) => {
+                        match serde_json::from_str::<InboundPayload>(&message) {
+                            Ok(payload) => {
+                                match payload.op {
+                                    InboundOpcode::Dispatch => {
+                                        println!("OPCODE 0 : Received Dispatch event");
+
+                                        if let Some(num) = payload.s {
+                                            let mut seq = read_seq_num.lock().await;
+                                            *seq = Some(num);
+
+                                            println!("Sequence updated: {}", num);
+                                        }
+                                    }
+                                    InboundOpcode::Reconnect => {}
+                                    InboundOpcode::InvalidSession => {}
+                                    InboundOpcode::Hello => {
+                                        println!("OPCODE 10 : Received Hello event");
+
+                                        tx.send(OutboundOpcode::Identify).await?;
+                                    
+                                        let heartbeat_interval = payload.d["heartbeat_interval"]
+                                            .as_u64()
+                                            .ok_or("Missing heartbeat_interval in Hello payload")?;
+
+                                        println!("Heartbeat interval: {}", heartbeat_interval);
+
+                                        let heartbeat_tx = tx.clone(); 
+                                        
+                                        tokio::spawn(async move {
+                                            let mut interval = tokio::time::interval(
+                                                std::time::Duration::from_millis(heartbeat_interval)
+                                            );
+
+                                            loop {
+                                                interval.tick().await;
+
+                                                println!("Sending heartbeat");
+                                                if let Err(e) = heartbeat_tx.send(OutboundOpcode::Heartbeat).await {
+                                                    eprintln!("Failed to send heartbeat to channel: {}", e);
+                                                    break; // 채널이 끊겼으므로 하트비트 루프 종료
+                                                }
+                                            }
+
+                                            Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+                                        });
+                                    }
+                                    InboundOpcode::HeartbeatAck => {}
+                                }
+                            }
+                            Err(error) => {
+                                eprintln!("Error parsing message: {}", error);
+                            }
+                        }
+                    }
+                    Ok(message) => {
+                        println!("Received non-text message: {:?}", message);
+                    }
+                    Err(error) => {
+                        eprintln!("Error reading message: {}", error);
+                        break;
+                    }
+                }
+            }
+
+            Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+        }); 
+
+        let _ = tokio::join!(listening_task, sending_task); 
         Ok(())
     }
 
     pub async fn send_payload<T: Serialize>(
         write: &mut SplitSink<
-            WebSocketStream<MaybeTlsStream<TcpStream>>,
+            WebSocketStream<tokio_tungstenite::MaybeTlsStream<TcpStream>>,
             Message
         >,
         payload: &OutboundPayload<T>,
@@ -64,94 +220,5 @@ impl Gateway {
             .await?;
 
         Ok(())
-    }
-
-    
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Serialize_repr, Deserialize_repr)]
-#[repr(u8)] 
-pub enum Opcode {
-    Dispatch = 0,
-    Heartbeat = 1,
-    Identify = 2,
-    PresenceUpdate = 3,
-    VoiceStateUpdate = 4,
-    Resume = 6,
-    Reconnect = 7,
-    RequestGuildMembers = 8,
-    InvalidSession = 9,
-    Hello = 10,
-    HeartbeatAck = 11,
-    RequestSoundboardSounds = 31,
-}
-
-
-// user -> discord
-#[derive(Debug, Serialize)]
-pub struct OutboundPayload<T> {
-    op: Opcode, 
-    d: T,
-}
-
-// discord -> user
-#[derive(Debug, Deserialize)]
-pub struct InboundPayload {
-    pub op: Opcode, 
-    pub d: serde_json::Value,
-    pub s: Option<u64>, 
-    pub t: Option<String>, 
-}
-
-// OPCOCDE 2 :: Identify
-#[derive(Debug, Serialize)]
-pub struct IdentifyData<'a> {
-    token: &'a str,
-    intents: u64,
-    properties: IdentifyProperties<'a>,
-}
-
-#[derive(Debug, Serialize)]
-pub struct IdentifyProperties<'a>{
-    os: &'a str,
-    browser: &'a str,
-    device: &'a str,
-}
-
-pub async fn send_payload<T: Serialize>(
-    write: &mut SplitSink<
-        WebSocketStream<MaybeTlsStream<TcpStream>>,
-        Message
-    >,
-    payload: &OutboundPayload<T>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let json = serde_json::to_string(payload)?;
-
-    write
-        .send(Message::Text(json.into()))
-        .await?;
-
-    Ok(())
-}
-
-pub fn create_identify(token: &str) -> OutboundPayload<IdentifyData<'_>> {
-    OutboundPayload {
-        op: Opcode::Identify, 
-        d: IdentifyData {
-            token: token, 
-            intents: 513, 
-            properties: IdentifyProperties {
-                os: "linux",
-                browser: "are-you-titti",
-                device: "are-you-titti",
-            },
-        }
-    }
-}
-
-pub fn create_heartbeat(seq: Option<u64>) -> OutboundPayload<Option<u64>> {
-    OutboundPayload {
-        op: Opcode::Heartbeat,
-        d: seq,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,15 @@
-use futures_util::{StreamExt};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
-use std::sync::Arc;
-use tokio::sync::Mutex;
-use crate::gateway::{send_payload};
+use crate::gateway::{Gateway};
 
 mod gateway; 
 
 
 async fn run(token: String) {
-    let url = "wss://gateway.discord.gg/?v=10&encoding=json";
+    let mut gateway = Gateway::new(token); 
 
-    println!("Connecting to Discord Gateway...");
-
-    // (1) Establish connection with gateway. 
-    match connect_async(url).await {
-        Ok((websocket, response)) => {
-            println!("Connected!");
-            println!("HTTP Status: {}", response.status()); 
-            
-            let (mut write, mut read) = websocket.split();
-            let (tx, mut rx) = tokio::sync::mpsc::channel(32);
-
-            let seq = Arc::new(Mutex::new(None::<u64>));
-            let read_seq = Arc::clone(&seq);
-            let write_seq = Arc::clone(&seq);
-
-            // [read task]
-            // 서버에서 온 정보를 읽고 데이터를 처리한 후, [write task] 에게 처리한 데이터 전달.
-            let read_task = tokio::spawn(async move {
-                while let Some(message) = read.next().await {
-                    match message {
-                        Ok(Message::Text(text)) => {
-                            match serde_json::from_str::<gateway::InboundPayload>(&text) {
-                                Ok(payload) => {
-                                    match payload.op {
-                                        gateway::Opcode::Dispatch => {
-                                            println!("OPCODE 0 : Recieved Dispatch event"); 
-
-                                                if let Some(s) = payload.s {
-                                                    let mut seq = read_seq.lock().await;
-                                                    *seq = Some(s);
-
-                                                    println!("Sequence updated: {}", s);
-                                                }
-                                        }
-                                        gateway::Opcode::Hello => {
-                                            println!("OPCODE 10 : Recieved Hello event"); 
-                                            
-                                            let heartbeat_interval = 
-                                                payload.d["heartbeat_interval"]
-                                                    .as_u64()
-                                                    .unwrap(); 
-                                            
-                                            let heartbeat_tx = tx.clone(); 
-
-                                            tokio::spawn(async move {
-                                                let mut interval = tokio::time::interval(
-                                                    std::time::Duration::from_millis(heartbeat_interval)
-                                                ); 
-                                                loop {
-                                                    interval.tick().await;
-
-                                                    println!("heartbeat interval"); 
-
-                                                    if heartbeat_tx
-                                                        .send(gateway::Opcode::Heartbeat)
-                                                        .await
-                                                        .is_err() {
-                                                            break;
-                                                        }
-                                                }
-                                            }); 
-
-                                            match tx.send(gateway::Opcode::Identify).await {
-                                                Ok(()) => (), 
-                                                Err(error) => eprintln!("{}", error), 
-                                            }
-                                        },
-                                        _ => println!("{:?}", payload), 
-                                    }
-                                }, 
-                                Err(error) => eprint!("Error: {}", error), 
-                            }
-                        }, 
-                        Ok(message) => println!("recieve: {}", message), 
-                        Err(error) => eprintln!("Result Error: {}", error), 
-                    }
-                }
-            }); 
-            
-            // [write task]
-            // [read task] 에서 온 정보를 읽고 데이터를 처리한 후, 서버에게 처리한 데이터 전달.
-            let write_task = tokio::spawn(async move {
-                while let Some(opcode) = rx.recv().await {
-                    match opcode {
-                        gateway::Opcode::Heartbeat => {
-                            let current_seq = {
-                                let seq = write_seq.lock().await;
-                                *seq
-                            };
-
-                            println!("OPCODE 1 : Sending Heartbeat"); 
-                            let payload = gateway::create_heartbeat(current_seq); 
-                            send_payload(&mut write, &payload).await.unwrap(); 
-                        }
-                        gateway::Opcode::Identify => {
-                            println!("OPCODE 2 : Sending Identify"); 
-                            let payload = gateway::create_identify(&token); 
-                            send_payload(&mut write, &payload).await.unwrap();
-                        },
-                        _ => println!(":("), 
-                    }
-                }
-            });
-
-            let _ = tokio::join!(read_task, write_task);
-        }, 
-        Err(error) => eprintln!("Gateway Connection Failed: {}", error), 
+    if let Err(e) = gateway.connect().await {
+        eprintln!("Error connecting to gateway: {}", e);
     }
 }
-
 
 #[tokio::main]
 async fn main() {
